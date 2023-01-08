f979d8db6e8ee9f9332a72ed3945089cb87d217f
Grecha <trafalgora@mail.ru>
2023-01-08 21:10:25 +0300

    First commit

---
+++ mysite/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+
+def about(request):
+    return HttpResponse('This is about Page')
+
+
+
+
+
+def home(request):
+    return render(request, 'home.html', {'greeting': 'hello'})
